a3153f9b799920884432bd246d94b8917a2f856f
jules009X <amenyoapedoh@gmail.com>
2025-01-09 11:35:22 +0100

    EXO 2

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,14 @@
+package personnages;
+
+public class Gaulois {
+	private String nom ;
+	private int force;
+    public Gaulois(String nom, int force){
+    	this.nom=nom;
+    	this.force=force;
+    }
+ // Getter pour le nom
+    public String getNom() {
+        return nom;
+    }
+}
