036c58e08781130f39bba1f2baff9f18c8fd5caa
jules009X <amenyoapedoh@gmail.com>
2025-01-16 08:27:44 +0100

    Suite du tp

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,14 +1,26 @@
 package personnages;
 
 public class Gaulois {
+	//Attributs 
 	private String nom ;
 	private int force;
+	
+	//Constructeur 
     public Gaulois(String nom, int force){
     	this.nom=nom;
     	this.force=force;
     }
  // Getter pour le nom
+    
     public String getNom() {
         return nom;
     }
+    public String parler(String texte) {
+    	return "Le gaulois "+ this.nom+": <<"+texte+">>";
+    }
+    public static void main(String[] args) {
+     Gaulois asterix = new Gaulois("Astérix",8);
+   	 System.out.println(asterix.getNom());
+   	 
+	}
 }
